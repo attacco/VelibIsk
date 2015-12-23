7feef651d6610f221da41c37507031a0ed192b65
attacco <nobody@example.com>
2015-12-23 20:27:29 +0300

    Added menu with "About" option

---
+++ app/src/main/java/com/example/velibisk/rssreader/AboutDialogFragment.java
@@ -0,0 +1,35 @@
+package com.example.velibisk.rssreader;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import javax.inject.Inject;
+
+/**
+ * Created by attacco on 23.12.2015.
+ */
+public class AboutDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+    @Inject
+    public AboutDialogFragment() {
+        super();
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity(), R.style.AppTheme_Dialog_Alert)
+                .setMessage(R.string.dialog_about_text)
+                .setPositiveButton(R.string.dialog_positive_button_text, this)
+                .create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        // here we have only one button
+    }
+}
--- app/src/main/java/com/example/velibisk/rssreader/MainActivity.java
+++ app/src/main/java/com/example/velibisk/rssreader/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.velibisk.rssreader.rss.RSSItem;
 
 import java.util.List;
-
-import butterknife.BindColor;
 
 /**
  * Created by attacco on 22.12.2015.
@@ -18,6 +19,7 @@ import butterknife.BindColor;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<RSSItem>>, FeedFragment.Listener {
     private final static String PROGRESS_FRAGMENT_TAG = "progress_fragment";
     private final static String FEED_FRAGMENT_TAG = "feed_fragment";
+    private final static String ABOUT_DIALOG_FRAGMENT_TAG = "about_dialog_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +34,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                showAboutDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showAboutDialog() {
+        getApplicationComponent().getAboutDialogFragment().show(getSupportFragmentManager(),
+                ABOUT_DIALOG_FRAGMENT_TAG);
+    }
+
+    @Override
     public Loader<List<RSSItem>> onCreateLoader(int id, Bundle args) {
-        return ((Application)getApplication()).getApplicationComponent().getFeedLoader();
+        return getApplicationComponent().getFeedLoader();
     }
 
     @Override
@@ -41,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         final FragmentManager fragmentManager = getSupportFragmentManager();
         FeedFragment fragment = (FeedFragment) fragmentManager.findFragmentByTag(FEED_FRAGMENT_TAG);
         if (fragment == null) {
-            fragment = ((Application)getApplication()).getApplicationComponent().getFeedFragment();
+            fragment = getApplicationComponent().getFeedFragment();
             fragment.setArguments(FeedFragment.createArguments(data));
             fragmentManager.beginTransaction()
                     .replace(R.id.fragmentContainer, fragment, FEED_FRAGMENT_TAG)
@@ -64,4 +89,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         getSupportLoaderManager().restartLoader(0, null, this);
     }
 
+    private ApplicationComponent getApplicationComponent() {
+        return ((Application) getApplication()).getApplicationComponent();
+    }
 }
