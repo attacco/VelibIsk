d8bf1a652dbe37bc175a818f51019aef151c5d3c
attacco <nobody@example.com>
2015-12-24 13:29:14 +0300

    bug fix

--- app/src/main/java/com/example/velibisk/rssreader/ui/MainActivity.java
+++ app/src/main/java/com/example/velibisk/rssreader/ui/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragmentContainer, new ProgressFragment(), PROGRESS_FRAGMENT_TAG)
+                .add(R.id.fragmentContainer, new ProgressFragment(), PROGRESS_FRAGMENT_TAG)
                 .commit();
 
         getSupportLoaderManager().initLoader(0, null, this);
